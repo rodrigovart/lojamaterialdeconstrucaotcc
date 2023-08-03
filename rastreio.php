331d890c97e13b79106b3626a1579288faa53cfe
AnaGi2005 <125368602+AnaGi2005@users.noreply.github.com>
2023-08-03 18:59:50 -0300

    Add files via upload

---
+++ rastreio.php
@@ -0,0 +1,20 @@
+<?php
+require '../header.php';
+require '../menutopo.php';
+?>
+<title>CONSTRUQUER</title>
+<form action="rastreio.php" method="post">
+
+
+<div class="col-md-4 position-relative">
+    <input type="text" class="form-control" placeholder="Digite seu código de rastreio" required >  
+    <button type="button" class="btn btn-primary mt-3 ">RASTREAR</button>
+   
+
+  </div>
+</div>
+
+
+  
+
+
