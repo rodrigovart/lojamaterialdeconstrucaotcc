0dfc7bd6877c7cd9e775583423bd366d5545625c
AnaGi2005 <125368602+AnaGi2005@users.noreply.github.com>
2023-08-03 18:56:35 -0300

    Add files via upload

---
+++ menutopo.php
@@ -0,0 +1,225 @@
+<div id="toponovo">
+<center>
+<!-- Topo UM -->
+<div id="topoum">
+  <div id="topoum-conteudo">
+    <div id="telefone"> (14) 3351-1312 </div>
+
+    <div id="email"> contato@construquer.com.br </div>
+    </a>
+    <div id="vaziaum"> </div>
+    <div id="textolinks"> <a href=""></a> </a> <a href="rastreio.php">Rastreio</a> <a href="sobrenos.php">Sobre nós</a> <a href="contato.php">Reclame aqui</a> </div>
+  </div>
+</div>
+<div id="topodois">
+  <center>
+	
+    <div id="topodois-conteudo"> 
+      <!-- LOGO -->
+      <div id="logo"><img src="../assets/imagens\imagenslogo48x48.png" width="180" height="70" </a> </div>
+        <!--barra de pesquisa-->
+  <div class="pesquisa">
+    <form class="example"  >
+  <input type="text" placeholder="Pesquisa..." name="search"  > 
+  <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="write" class="bi bi-person-fill" viewBox="0 0 16 16">
+  <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/>
+
+</svg>
+<svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-cart-fill" viewBox="0 0 16 16">
+  <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+</svg>
+</form>
+
+  </div>
+ 
+    </div>
+  </center>
+</div>
+<div class="mt-3">
+        
+</svg>
+<style>
+ 
+  </style>
+  
+  
+<style>
+  /* Topo UM */
+#topoum {
+	height:37px;
+	width:100%;
+	background:white;
+	margin:0 auto;
+	padding:0px;	
+}
+
+#topoum-conteudo{
+	height:37px;
+	width:1100px;
+	background: white;
+	margin:0 auto;	
+}
+
+#topoum #textolinks a{
+	padding:11px 15px 10px 10px;
+	font-family: Helvetica, Arial, Verdana, sans-serif;
+	font-size:12px;
+	text-decoration:none;
+	float: right;
+	display:block;
+	color:00;
+	margin:0 auto;
+}
+
+
+#topoum #textolinks a:hover{text-decoration:underline;}
+
+#topoum #email{
+	padding:11px 50px 8px 40px;
+	font-family: Helvetica, Arial, Verdana, sans-serif;
+	font-size:12px;
+	text-decoration:none;
+	float: right;
+	color:grey;
+	margin:0px;
+	background: url(https://ssl267.websiteseguro.com/asterplas/loja-de-embalagens/site/image/email.png) no-repeat bottom left;
+	background-position:0px 4px;
+	
+}
+
+#topoum #telefone{
+	padding:11px 5px 8px 40px;
+	font-family: Helvetica, Arial, Verdana, sans-serif;
+	font-size:12px;
+	text-decoration:none;
+	float: right;
+	color:grey;
+	margin:0px;
+	background: url(https://ssl267.websiteseguro.com/asterplas/loja-de-embalagens/site/image/tel.png) no-repeat  left;
+	background-position: 0px 4px;
+}
+
+#topoum #vaziaum{
+	width:70px;
+	height:25px;
+	float:right;
+}
+
+/* Topo DOIS */
+#topodois{
+	height:100px;
+	width:100%;
+	background-image : linear-gradient(to right, rgb(20,147,220), rgb(17,54,71));
+	margin: 0 auto;
+}
+
+#topodois-conteudo{
+	height:100px;
+	width: 1100px;
+	background-image : linear-gradient(to right, rgb(20,147,220), rgb(17,54,71));
+	margin: 0 auto;	
+}
+
+#logo{
+	padding:17px;
+	float:left;
+	margin:0px;
+	}
+
+/*barra de pesquisa*/
+.pesquisa{
+  left: 25%;
+  width: 35%;
+  position: absolute;
+  top: 9%;
+ 
+}
+
+form.example input[type=text] {
+  padding: 8px;
+  font-size: 17px;
+  border: 1px solid grey;
+  width: 60%;
+  background: #f1f1f1;
+}
+
+form.example button {
+  width: 7%;
+  right: 9%;
+  padding: 8px;
+  background: white;
+  color: black;
+  font-size: 17px;
+  border: 1px solid grey;
+  border-left: none;
+  cursor: pointer;
+}
+
+form.example button:hover {
+  background: #2196F3;
+}
+
+form.example::after {
+  content: "";
+  clear: both;
+  display: table;
+}
+/*fim da barra de pesquisa*/
+
+#topodois #form-wrap { 
+	width:435px;
+	height:37px;
+	padding: 5px;
+	vertical-align: middle;
+	overflow: hidden;
+	float:left;
+	clear:left;
+	border: solid 0px;
+}
+
+#topodois .form-text { 
+	width:390px;
+	height:30px;
+	margin:auto;
+	padding:0px;
+	color:#b1b1b1;
+	border:0px;
+	float:left;
+	background:trasnparent;
+
+}
+
+
+
+
+#topodois #login{
+	
+	height:40px;
+	background-position: 0px 7px;
+	float:right;
+	color:white;
+	font-size: 12px;
+	font-family:Gotham, "Helvetica Neue", Helvetica, Arial, sans-serif;
+	text-align:left;
+	padding:7px 25px 0px 40px;
+
+}
+
+
+
+#topodois #loja{
+	
+	height:40px;
+	background-position: 0px 7px;
+	float:right;
+	color:white;
+	font-size: 12px;
+	font-family:Gotham, "Helvetica Neue", Helvetica, Arial, sans-serif;
+	text-align:left;
+	padding:7px 25px 0px 40px;
+
+}
+
+
+</style>
+</nav>
