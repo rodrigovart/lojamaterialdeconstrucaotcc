8264ef22b9e4375b697ce2f95b4833f6f74ae807
AnaGi2005 <125368602+AnaGi2005@users.noreply.github.com>
2023-08-03 18:59:12 -0300

    Add files via upload

---
+++ sobrenos.php
@@ -0,0 +1,29 @@
+
+<?php
+require '../header.php';
+require '../menutopo.php';
+?>
+<title>CONSTRUQUER</title>
+
+</style>
+<div class="mt-3">
+<h1 style="text-align:center; font-size: 28px;font-weight:200; color text-align: center; font-size: 28px;font-weight: 200; color: #3A5578; margin-bottom: 0px; ">
+Tudo na
+<strong>CONSTRUQUER</strong>
+  </h1>
+  <p style="text-align:center;">Entregamos estilo e qualidade, no prazo e preço justo</p>
+
+  <div class="card" style="width: 20rem; right: 22">
+  <div class="card-body">
+    
+    <h5 class="card-title"> <h1 style="text-align:center; font-size: 30px;font-weight:200; color text-align: center; font-size: 28px;font-weight: 200; color: #3A5578; margin-bottom: 0px; "> Mais que uma loja</h5> 
+    
+    <p class="card-text">Na Loja CONSTRUQUER você encontra tudo que precisa para construir, reformar ou decorar com as melhores opções do mercado. São muitos produtos à pronta entrega e uma experiência de compra com profissionais qualificados, para te ajudar com o produto mais adequado para sua necessidade e um preço que cabe no seu bolso.</p>
+  </div>
+</div>
+
+
+
+
+
+
